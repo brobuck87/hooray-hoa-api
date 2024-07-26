e4a4da91f8d45a8dd3d74ca0b6f76a3b0894043d
John Sickels <johnwsickels@gmail.com>
2024-07-25 21:53:43 -0400

    build(app): add linter and gh workflow

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,10 +1,9 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 export const errorHandler = (
     error: Error,
     req: Request,
-    res: Response,
-    next: NextFunction
+    res: Response
 ) => {
     console.error(`Error: ${error.message}`);
     return res.status(500).json({ message: "Internal server error" });
